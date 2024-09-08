a75bb453679573387f1551ba53cdf1a25d5bda16
Udara Sampath <udarasampathw196841@gmail.com>
2024-09-08 15:13:13 +0530

    init

---
+++ AQD_HUB/AQD_HUB/AQD_HUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQD_HUBApp.swift
+//  AQD_HUB
+//
+//  Created by Udara Sampath on 2024-09-08.
+//
+
+import SwiftUI
+
+@main
+struct AQD_HUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AQD_HUB/AQD_HUB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AQD_HUB
+//
+//  Created by Udara Sampath on 2024-09-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

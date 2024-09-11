318c28b229eed3c391855293424c3ca4d0f7d4b3
Udara Sampath <udarasampathw196841@gmail.com>
2024-09-11 17:30:19 +0530

    remove cmd warning

--- AQD-Heading-Hub-IOS/CommandsView.swift
+++ AQD-Heading-Hub-IOS/CommandsView.swift
@@ -17,91 +17,64 @@ struct CommandsView: View {
 
     var body: some View {
         VStack {
-            if networkService.connectedIP == "192.168.4.3" {
-                Text("Control ESP8266")
-                    .font(.title)
-                    .padding()
+            Text("Control ESP8266")
+                .font(.title)
+                .padding()
 
-                // Buttons to control the ESP8266
-                HStack(spacing: 20) {
-                    Button(action: { sendCommand("FORWARD") }) {
-                        Text("Forward")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-
-                    Button(action: { sendCommand("BACKWARD") }) {
-                        Text("Backward")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                }
-
-                HStack(spacing: 20) {
-                    Button(action: { sendCommand("LEFT") }) {
-                        Text("Left")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-
-                    Button(action: { sendCommand("RIGHT") }) {
-                        Text("Right")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-
-                    Button(action: { sendCommand("STOP") }) {
-                        Text("Stop")
-                            .padding()
-                            .background(Color.red)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                }
-
-                // Display the result of the command
-                Text("Command Result: \(commandResult)")
-                    .padding()
-                    .foregroundColor(.green)
-            } else {
-                // Show message if not connected to correct Wi-Fi
-                VStack {
-                    Text("Please connect to 'AQD HUB' Wi-Fi and try again.")
-                        .font(.headline)
+            // Buttons to control the ESP8266
+            HStack(spacing: 20) {
+                Button(action: { sendCommand("FORWARD") }) {
+                    Text("Forward")
                         .padding()
-                    Button(action: {
-                        // Dismiss view to go back
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("Go Back")
-                            .padding()
-                            .background(Color.red)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                Button(action: { sendCommand("BACKWARD") }) {
+                    Text("Backward")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                 }
             }
+
+            HStack(spacing: 20) {
+                Button(action: { sendCommand("LEFT") }) {
+                    Text("Left")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                Button(action: { sendCommand("RIGHT") }) {
+                    Text("Right")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                Button(action: { sendCommand("STOP") }) {
+                    Text("Stop")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+
+            // Display the result of the command
+            Text("Command Result: \(commandResult)")
+                .padding()
+                .foregroundColor(.green)
         }
         .padding()
         .onAppear {
             // Load power settings from UserDefaults
             loadPowerSettings()
-
-            // Only show the alert if the IP is incorrect
-            if networkService.connectedIP != "192.168.4.3" {
-                showAlert = true
-                alertMessage = "You are not connected to 'AQD HUB' Wi-Fi. Please connect and try again."
-            } else {
-                showAlert = false  // No need to show alert if connected to correct IP
-            }
         }
         .alert(isPresented: $showAlert) {
             Alert(title: Text("Connection Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
